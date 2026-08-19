c0687f37872d18ccfcdfbebce22910017619ef63
Yukesh H K <yukesh6002hk@gmail.com>
2026-08-19 10:16:14 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class exception {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		try {
+			System.out.println(10 / 2);
+			int[] a = { 10, 20, 30 };
+			System.out.println(a[0]);
+			System.out.println(a[1]);
+			System.out.println(a[2]);
+			System.out.println(a[3]);
+
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(" Array handle");
+
+		} catch (ArithmeticException e) {
+			System.out.println(" arthi handle");
+		System.out.println("Hello sir");
+		} catch (Exception e) {
+			System.out.println(" exception handle");
+		}
+	}
+
+}
