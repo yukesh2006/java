59ce94c95a30cae4b5b096531cccc4d2af415a7e
Yukesh H K <yukesh6002hk@gmail.com>
2026-09-04 16:01:11 +0530

    Delete Multiplication.java

--- Multiplication.java
+++
@@ -1,7 +0,0 @@
-public class Multiplication {
-    public static void main(String[] args) {
-        int a=10;
-        int b=5;
-        System.out.println("Multiplication = " + (a * b));
-    }
-}
